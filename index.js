ca49d329862876117b1a401cc9e1a05a6baebe81
Acesif <asifpalak@gmail.com>
2023-03-20 16:27:37 +0600

    3/20/23

--- index.js
+++ index.js
@@ -9,17 +9,26 @@ let day = weekday[d.getDay()];
 const showDate = document.getElementById("date");
 showDate.innerText = `${day} : ${date}-${month}-${year}`;
 
-let checkMouse = () => {
-    document.addEventListener('mousemove',(e)=>{
-        if(e.pageY>970){
-            document.getElementById("nameplate").style["opacity"] = "0";
-        }
-        else{
-            document.getElementById("nameplate").style["opacity"] = "1"; 
-        }
-    })
+let nameplate = document.getElementById("nameplate");
+
+let ret = document.getElementById("return");
+
+window.onscroll = () => {
+    scrollFunction()
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    ret.style.display = "block";
+    nameplate.style.opacity = "0";
+  } else {
+    ret.style.display = "none";
+    nameplate.style.opacity = "1";
+  }
 }
-// document.setInterval(checkMouse(), 1);
-window.onscroll = () =>{
-    console.log(document.body.scrollHeight);
-}
+
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0;
+} 
+ret.addEventListener('click',topFunction);
